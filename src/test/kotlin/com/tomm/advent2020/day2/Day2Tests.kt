8e2177613ef3687b5220e68a8faf0a72f788332e
= <admin@indovaps.com>
2020-12-03 05:11:50 -0500

    Fix Day2 xor condition

--- src/test/kotlin/com/tomm/advent2020/day2/Day2Tests.kt
+++ src/test/kotlin/com/tomm/advent2020/day2/Day2Tests.kt
@@ -5,24 +5,20 @@ import org.junit.jupiter.api.Test
 
 class Day2Tests {
 
+    private val input = sequenceOf(
+        "1-3 a: abcde",
+        "1-3 b: cdefg",
+        "2-9 c: ccccccccc"
+    )
+
     @Test
     fun testPart1() {
-        val input = sequenceOf(
-            "1-3 a: abcde",
-            "1-3 b: cdefg",
-            "2-9 c: ccccccccc"
-        )
         val passwords = PasswordListParser().parse(input)
         Assertions.assertEquals("2", Day2.steps[0].solve(passwords))
     }
 
     @Test
     fun testPart2() {
-        val input = sequenceOf(
-            "1-3 a: abcde",
-            "1-3 b: cdefg",
-            "2-9 c: ccccccccc"
-        )
         val passwords = PasswordListParser().parse(input)
         Assertions.assertEquals("1", Day2.steps[1].solve(passwords))
     }
